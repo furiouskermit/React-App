48e4594f4ed4e8c27eb4be79032fbf8b5ae5f84c
furiouskermit <vndnsla12@gmail.com>
2022-04-24 01:05:39 +0900

    220424 01:05

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [value, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i can do this all day");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1 className={styles.title}>Welcome Back!!!!!</h1>
       <Button text={"즐겁다"} />
+      <div>{value}</div>
+      <button onClick={onClick}>adsasddas</button>
     </div>
   );
 }
