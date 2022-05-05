88db3e097da8edf85f6701f75bd8cfe262a1a3e2
furiouskermit <vndnsla12@gmail.com>
2022-05-05 22:30:38 +0900

    useEffect 220505

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [counter, SetValue] = useState(0);
+  const [keyword, steKeyword] = useState("");
+  const onClick = () => SetValue((prev) => prev + 1);
+  const onChange = (event) => steKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); //한 번만 실행
+  useEffect(() => {
+    //처음 시작할 때도 발생하는 현상 방지 
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("search for", keyword)
+    }
+    console.log("i run when 'keyword' changes")
+  }, [keyword]) //keyword 변경될 때만 실행
+  useEffect(() => {
+    console.log("i run when 'counter' changes")
+  }, [counter]) //counter 변경될 때만 실행
+
+  //즉 useEffcet()를 사용해 원하는 코드를 원하는 상황에만 쓸 수 있게 함
+  //useEffect에서 [객체]는 객체의 변화를 감지하면 코드 실행한다는 뜻
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!!!</h1>
-      <Button text={"즐겁다"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search Here..." /> 
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
