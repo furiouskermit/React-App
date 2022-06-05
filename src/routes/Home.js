bc3af810241d58a67effdef6f4e0d8f27aef1e1f
furiouskermit <vndnsla12@gmail.com>
2022-06-05 17:59:57 +0900

    220605

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,42 +1,32 @@
-import Button from "../Button";
-import styles from "../App.module.css"
-import { func } from "prop-types";
-import { type } from "@testing-library/user-event/dist/type";
-import { useState, useEffect } from "react"
-import Movie from "../components/Movie";
+import {useState, useEffect} from "react"
+
 
 function Home() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  //async-await은 then과 같은 역할이며 async-await이 좀 더 보기 좋은 형태이다.
-  const getMovies = async() => {
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+  const [homeLoading, setHomeLoading] = useState(true);
+  const [news, setNews] = useState([]);
+  const getNews = async() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'bbc-news-mundo.p.rapidapi.com',
+        'X-RapidAPI-Key': '9af7881081msh604f31bae7bd0a3p1f44aajsn1847b34e4aaa'
+      }
+    };
+    const response = await fetch("https://bbc-news-mundo.p.rapidapi.com/news", options);
     const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
+    console.log(json)
+    setHomeLoading(false);
   }
-  //▼한 번만 실행
   useEffect(() => {
-    getMovies();
+    getNews();
   }, [])
-  //console.log(movies)
   return <div>
-    {loading ? <h1>Loading...</h1> : 
-      <div>{movies.map((movie) => (
-        <Movie 
-          key={movie.id}
-          id={movie.id} 
-          coverImg={movie.medium_cover_image} 
-          title={movie.title} 
-          summary={movie.summary} 
-          genre={movie.genre} 
-        />
-      )
-      //property로 보내줘야 Movie.js가 받음
-      )}</div>
+    {homeLoading ? <h1>Loading...</h1> : 
+      <div>
+        
+      </div>
     }
-  </div>;
-
+  </div>
 }
 
-export default Home;
+export default Home
