9b249336df7ec5a48f4ea80809989c9d2703b948
furiouskermit <vndnsla12@gmail.com>
2022-06-13 23:45:12 +0900

    220613

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   //Switch : Route를 찾고 찾으면 그것을 component로 만드는 역할
   return <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />}></Route>
-      <Route path="/" element={<Home />}></Route>
+      <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />}></Route>
+      <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}></Route>
     </Routes>
   </Router>;
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import styles from "../App.module.css"
+import {useState} from "react"
 
 function Header() {
+  const homePath = document.location.pathname === "/React-App";
+  const [reverse, setReverse] = useState(true);
+  const onClick = () => setReverse((current) => !current);
   return <header className={styles.header}>
-    <h1 className={styles.headerTitle}>News</h1>
+    <h1 className={styles.headerTitle}>{homePath ? "Kermit the Furious" : "NEWS" }</h1>
+    <button onClick={onClick} className={reverse ? `${styles.trigger}` : `${styles.dNone}`}><span></span><span></span><span></span></button>
+    <div className={reverse ? `${styles.headerMenu}` : `${styles.headerMenuActive}`} id="headerMenu">
+      <button type="button" onClick={onClick} className={styles.btnClose}><i className="xi-close"></i></button>
+      <h3 className={styles.welcomeText}>Welcome!</h3>
+      <ul className={styles.headerUl}>
+        <li><a href="#none">Home</a></li>
+        <li><a href="#none">Movie</a></li>
+        <li><a href="#none">News</a></li>
+        <li><a href="#none">To Do List</a></li>
+      </ul>
+    </div>
   </header>
 }
 
